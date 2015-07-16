47973090315f1e6f39b746289b23192634725cb9
DmitrySigaev <dsigaev@yandex.ru>
2015-07-16 18:39:40 +0300

    Testing of Reading MCD43A4 hdf files

---
+++ tests/testMCD43A4.m
@@ -0,0 +1,28 @@
+clear all;
+close all;
+
+mfn = mfilename;
+version = 'ver# 2015.07.16';
+disp(char(['-> ' mfn ' ' version]));
+
+
+addpath ..
+
+[files, dirs] = findallfiles( '..', 'r' );
+dirs = dirs';
+files = files';
+expr = '.hdf$';
+[t m] = regexpi(files, expr ,'tokens', 'match', 'lineanchors') ;
+isdef = @(x) (~isempty(x));
+log = double(cellfun(isdef,m));
+
+index = find(log ==1);
+
+input_f = files(index);
+
+DOY_A4_file_name = input_f{1};
+[red_d, nir_d, blue_d, green_d, scale, offset, fillval] = read_data_from_a4(DOY_A4_file_name);
+
+disp(scale);
+
+disp(char(['<- ' mfn ' ' version]));
