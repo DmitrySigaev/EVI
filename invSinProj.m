6a94c7156671407b6c882782bec25d919e7d8c66
DmitrySigaev <dsigaev@yandex.ru>
2015-07-20 18:09:56 +0300

    invSinProj transform and testing

---
+++ invSinProj.m
@@ -0,0 +1,9 @@
+ function [lat, long] = invSinProj(x,y) 
+	 R = 6371007.18100; %Earth's radius
+     long_0 = 0;
+	 lat = y/R;
+	 long = long_0 + x/(R*cos(lat));
+
+	 lat = lat*180/pi; %degree
+	 long = long*180/pi;
+ end 
