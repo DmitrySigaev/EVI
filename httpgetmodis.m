2e4fdce7f5f6d4fdba483ab0780ab6e9c2868ff9
DmitrySigaev <dsigaev@yandex.ru>
2015-07-28 20:33:23 +0300

    fix

--- httpgetmodis.m
+++ httpgetmodis.m
@@ -5,6 +5,8 @@ mfn = mfilename;
 version = 'ver# 2015.07.28';
 disp(char(['-> ' mfn ' ' version]));
 
+%str = urlread('http://e4ftl01.cr.usgs.gov//MODIS_Composites/MOTA/MCD43A4.005/');
+
 %parse data_url_script_2006-2007.txt 
 fid = fopen('data_url_script_2006-2007.txt');
 
@@ -17,7 +19,7 @@ while ischar(tline)
     [t_years_days m_files_name] = regexpi(tline, expr ,'tokens', 'match', 'lineanchors') ;  
     disp(['file: ' m_files_name{1,1}]);
     disp(['year: ' t_years_days{1,1}{1,1}]);
-    disp(['mounth: ', t_years_days{1,1}{1,2}]);
+    disp(['month: ', t_years_days{1,1}{1,2}]);
     disp(['day: ', t_years_days{1,1}{1,3}]);
     disp(['DOY: ', t_years_days{1,1}{1,4}]);
     disp(['tile: ', t_years_days{1,1}{1,5}]);
