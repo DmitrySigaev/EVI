65b122cd59411a721f9082d1a08c8da9cf23c625
DmitrySigaev <dsigaev@yandex.ru>
2015-07-29 23:35:21 +0300

    parse data_url_script_2006-2007.txt using fread

--- httpgetmodis.m
+++ httpgetmodis.m
@@ -2,17 +2,38 @@ clear all;
 close all;
 
 mfn = mfilename;
-version = 'ver# 2015.07.28';
+version = 'ver# 2015.07.29';
 disp(char(['-> ' mfn ' ' version]));
 
 %str = urlread('http://e4ftl01.cr.usgs.gov//MODIS_Composites/MOTA/MCD43A4.005/');
 
 %parse data_url_script_2006-2007.txt 
+%% using fread
 fid = fopen('data_url_script_2006-2007.txt');
 
 expr  = 'http://e4ftl01.cr.usgs.gov//MODIS_Composites/MOTA/MCD43A4.005/([0-9]{4}).([0-9]{2}).([0-9]{2})/MCD43A4.A[0-9]{4}([0-9]{3}).([^:]{6}).[^:]+.hdf$';
 
-%line by line
+all_f = fread(fid, '*char')';
+
+[t_years_days m_files_name] = regexpi(all_f, expr ,'tokens', 'match', 'lineanchors') ;  
+
+for i = 1:length(t_years_days)
+    disp(['file: ' m_files_name{1,i}]);
+    disp(['year: ' t_years_days{1,i}{1,1}]);
+    disp(['month: ', t_years_days{1,i}{1,2}]);
+    disp(['day: ', t_years_days{1,i}{1,3}]);
+    disp(['DOY: ', t_years_days{1,i}{1,4}]);
+    disp(['tile: ', t_years_days{1,i}{1,5}]);
+end
+
+fclose(fid);
+
+%% line by line
+fid = fopen('data_url_script_2006-2007.txt');
+
+expr  = 'http://e4ftl01.cr.usgs.gov//MODIS_Composites/MOTA/MCD43A4.005/([0-9]{4}).([0-9]{2}).([0-9]{2})/MCD43A4.A[0-9]{4}([0-9]{3}).([^:]{6}).[^:]+.hdf$';
+
+
 tline = fgetl(fid);
 disp('Reading...');
 while ischar(tline)
